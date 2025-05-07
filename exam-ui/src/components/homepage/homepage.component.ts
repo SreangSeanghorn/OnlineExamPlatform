b43bdac6466a63a0d99c0c42efa223187a0d7280
SreangSeanghorn <s.seanghorn123@gmail.com>
2025-05-06 21:04:09 -0500

    updated hompage UI & fix login and register UI

--- exam-ui/src/components/homepage/homepage.component.ts
+++ exam-ui/src/components/homepage/homepage.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
 import {MatCard} from '@angular/material/card';
 import {MatIcon} from '@angular/material/icon';
-import {RouterLink} from '@angular/router';
+import {RouterLink, RouterModule} from '@angular/router';
 import {MatAnchor, MatButton} from '@angular/material/button';
 import {MatToolbar} from '@angular/material/toolbar';
 import {MatFormField, MatLabel} from '@angular/material/form-field';
 import {MatInput} from '@angular/material/input';
 import {FormsModule} from '@angular/forms';
+import { LoginComponent } from "../auth/login/login.component";
+import { NgForOf } from '@angular/common';
+import { RegisterComponent } from '../auth/register/register.component';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-homepage',
@@ -20,22 +24,37 @@ import {FormsModule} from '@angular/forms';
     MatInput,
     MatAnchor,
     MatLabel,
-    FormsModule
-  ],
+    FormsModule,
+    LoginComponent,
+    RouterLink,
+    RouterModule,
+    NgForOf
+],
   templateUrl: './homepage.component.html',
-  styleUrls: ['./homepage.component.scss','src/theme.scss']
+  styleUrls: ['./homepage.component.scss']
 })
 export class HomepageComponent {
+  tabs = [
+    { name: 'Home', route: '/home' },
+    { name: 'About', route: '/about' },
+    { name: 'Register', route: null },
+  ];
 
   username='';
   password='';
-  onLogin(){
-
-  }
+  constructor(private dialog: MatDialog) {}
   loginValidation(username:string,password:string){
     // validate login
     if (username === '' || password === ''){
 
     }
+  }
+  openRegister(event: Event): void {
+    event.preventDefault(); // prevent default navigation behavior
+    this.dialog.open(RegisterComponent, {
+      width: '500px',
+      autoFocus: false,
+      disableClose: false
+    });
   }
 }
